0d4565f9e080ae2e4853f68f2a32812c7e2255ab
Brattelnik <www.Brattelnik@mail.ru>
2015-10-13 12:02:59 +0300

    Added some files to Practice7

---
+++ Practice7/practice7.3.py
@@ -0,0 +1,6 @@
+__author__ = 'Ivan'
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.arange(-10,10.01,0.01)
+plt.plot(x,np.log((x**2+1)*np.exp(-abs(x)/10),1+np.tan(1/(1+np.sin(x)*np.sin(x)))))
+plt.show()
